1261a0f7f855729bfc22aa441ea3413f68da8bca
Altaire13 <altaire13@yandex.ru>
2018-09-19 04:44:28 +0300

    Add local cuda gpg key

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -7,6 +7,7 @@ fi
 function install_cuda_linux()
 {
     wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+    sudo apt-key add /var/cuda-repo-9-0/7fa2af80.pub
     sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
     sudo apt-get update
     sudo apt-get install cuda
