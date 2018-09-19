51f45929f2c63b525b44e9eaeec093f29ebed506
Altaire13 <altaire13@yandex.ru>
2018-09-19 04:11:00 +0300

    Change cuda version to 9.0

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -6,8 +6,8 @@ fi
 
 function install_cuda_linux()
 {
-    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
-    sudo dpkg -i cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
+    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
     sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
     sudo apt-get update
     sudo apt-get install cuda    
@@ -16,7 +16,7 @@ function install_cuda_linux()
 
 if [ "${CB_BUILD_AGENT}" == 'clang-linux-x86_64-release-cuda' ]; then
     install_cuda_linux;
-    ./ya make --no-emit-status --stat -T -r -j 1 catboost/app -DCUDA_ROOT=/usr/local/cuda-9.2 -DNO_DEBUGINFO;
+    ./ya make --no-emit-status --stat -T -r -j 1 catboost/app -DCUDA_ROOT=/usr/local/cuda-9.0 -DNO_DEBUGINFO;
     cp $(readlink -f catboost/app/catboost) catboost-cuda-linux;
     python ci/webdav_upload.py catboost-cuda-linux
 fi
@@ -24,7 +24,7 @@ fi
 if [ "${CB_BUILD_AGENT}" == 'python2-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
-     python2 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.2 ;
+     python2 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 ;
      python ../../ci/webdav_upload.py *.whl
 fi
 
@@ -33,7 +33,7 @@ if [ "${CB_BUILD_AGENT}" == 'python34-linux-x86_64-release' ]; then
      ln -s /home/travis/virtualenv/python3.4.6/bin/python-config /home/travis/virtualenv/python3.4.6/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.2 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.4.6/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.4.6/bin/python3-config;
      python ../../ci/webdav_upload.py *.whl
 fi
 
@@ -41,7 +41,7 @@ if [ "${CB_BUILD_AGENT}" == 'python35-linux-x86_64-release' ]; then
      ln -s /home/travis/virtualenv/python3.5.5/bin/python-config /home/travis/virtualenv/python3.5.5/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.2 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.5/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.5/bin/python3-config;
      python ../../ci/webdav_upload.py *.whl
 fi
 
@@ -49,7 +49,7 @@ if [ "${CB_BUILD_AGENT}" == 'python36-linux-x86_64-release' ]; then
      ln -s /home/travis/virtualenv/python3.6.3/bin/python-config /home/travis/virtualenv/python3.6.3/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.2 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
      python ../../ci/webdav_upload.py *.whl
 fi
 
