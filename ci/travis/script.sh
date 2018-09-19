edc0c9bb288b65ae820e44a955e1baea1a0d1ccc
Altaire13 <altaire13@yandex.ru>
2018-09-19 04:32:30 +0300

    Fix debian install

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -7,7 +7,7 @@ fi
 function install_cuda_linux()
 {
     wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
-    sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
     sudo apt-get update
     sudo apt-get install cuda
 }
