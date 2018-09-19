960faf174d58869af26eaf095a936d2a4717ad86
Altaire13 <altaire13@yandex.ru>
2018-09-19 04:25:57 +0300

    Use cuda local installation

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -6,11 +6,10 @@ fi
 
 function install_cuda_linux()
 {
-    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-    sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-    sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+    wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+    sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64.deb
     sudo apt-get update
-    sudo apt-get install cuda-9-0    
+    sudo apt-get install cuda
 }
 
 
