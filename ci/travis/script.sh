c1dcfe02c6b43f0d45bde6e39a1430f7f04914bd
Aleksei Gradskov <gradksov@yandex-team.ru>
2018-09-21 19:48:19 +0300

    Restore webdav

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -18,14 +18,14 @@ if [ "${CB_BUILD_AGENT}" == 'clang-linux-x86_64-release-cuda' ]; then
     install_cuda_linux;
     ./ya make --no-emit-status --stat -T -r -j 1 catboost/app -DCUDA_ROOT=/usr/local/cuda-9.0 -DNO_DEBUGINFO;
     cp $(readlink -f catboost/app/catboost) catboost-cuda-linux;
-    #python ci/webdav_upload.py catboost-cuda-linux
+    python ci/webdav_upload.py catboost-cuda-linux
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python2-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
      python2 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 ;
-     #python ../../ci/webdav_upload.py *.whl
+     python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python34-linux-x86_64-release' ]; then
@@ -34,7 +34,7 @@ if [ "${CB_BUILD_AGENT}" == 'python34-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
      python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.4.6/bin/python3-config;
-     #python ../../ci/webdav_upload.py *.whl
+     python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python35-linux-x86_64-release' ]; then
@@ -42,7 +42,7 @@ if [ "${CB_BUILD_AGENT}" == 'python35-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
      python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.5/bin/python3-config;
-     #python ../../ci/webdav_upload.py *.whl
+     python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python36-linux-x86_64-release' ]; then
@@ -50,13 +50,13 @@ if [ "${CB_BUILD_AGENT}" == 'python36-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
      python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
-     #python ../../ci/webdav_upload.py *.whl
+     python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'clang-darwin-x86_64-release' ]; then
     ./ya make --no-emit-status --stat -T -r -j 1 catboost/app;
     cp $(readlink catboost/app/catboost) catboost-darwin;
-    #python ci/webdav_upload.py catboost-darwin
+    python ci/webdav_upload.py catboost-darwin
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'R-clang-darwin-x86_64-release' ] || [ "${CB_BUILD_AGENT}" == 'R-clang-linux-x86_64-release' ]; then
@@ -80,6 +80,6 @@ if [ "${CB_BUILD_AGENT}" == 'R-clang-darwin-x86_64-release' ] || [ "${CB_BUILD_A
     cp $(readlink src/libcatboostr.so) catboost/inst/libs
 
     tar -cvzf catboost-R-$(uname).tgz catboost
-    #python ../../ci/webdav_upload.py catboost-R-*.tgz
+    python ../../ci/webdav_upload.py catboost-R-*.tgz
 fi
 
