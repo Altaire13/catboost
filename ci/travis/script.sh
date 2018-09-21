8ccb54baeec015019cbc1db78cb4392790d18a43
Aleksei Gradskov <gradksov@yandex-team.ru>
2018-09-22 01:02:35 +0300

    Fix cuda and python versions and add python-pycurl

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -41,7 +41,7 @@ if [ "${CB_BUILD_AGENT}" == 'python35-linux-x86_64-release' ]; then
      ln -s /home/travis/virtualenv/python3.5.6/bin/python-config /home/travis/virtualenv/python3.5.6/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.5/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.6/bin/python3-config;
      python ../../ci/webdav_upload.py *.whl
 fi
 
@@ -49,7 +49,7 @@ if [ "${CB_BUILD_AGENT}" == 'python36-linux-x86_64-release' ]; then
      ln -s /home/travis/virtualenv/python3.6.3/bin/python-config /home/travis/virtualenv/python3.6.3/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
      python ../../ci/webdav_upload.py *.whl
 fi
 
