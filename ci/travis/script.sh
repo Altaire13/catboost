870aa583f0ce0f93cd5bf2ce8f479de3d8b488e2
Aleksei Gradskov <gradksov@yandex-team.ru>
2018-09-21 18:24:54 +0300

    Disable webdav

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -25,7 +25,7 @@ if [ "${CB_BUILD_AGENT}" == 'python2-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
      python2 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 ;
-     python ../../ci/webdav_upload.py *.whl
+     #python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python34-linux-x86_64-release' ]; then
@@ -34,14 +34,14 @@ if [ "${CB_BUILD_AGENT}" == 'python34-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
      python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.4.6/bin/python3-config;
-     python ../../ci/webdav_upload.py *.whl
+     #python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python35-linux-x86_64-release' ]; then
      ln -s /home/travis/virtualenv/python3.5.5/bin/python-config /home/travis/virtualenv/python3.5.5/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.5/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.5/bin/python3-config;
      #python ../../ci/webdav_upload.py *.whl
 fi
 
@@ -49,7 +49,7 @@ if [ "${CB_BUILD_AGENT}" == 'python36-linux-x86_64-release' ]; then
      ln -s /home/travis/virtualenv/python3.6.3/bin/python-config /home/travis/virtualenv/python3.6.3/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-9.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
      #python ../../ci/webdav_upload.py *.whl
 fi
 
